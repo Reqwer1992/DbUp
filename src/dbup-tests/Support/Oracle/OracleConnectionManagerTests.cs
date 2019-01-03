e89293feb35a1e015f162d7327020ae7e34fdfaf
Ēriks Krūmiņš <eriks.krumins@if.lv>
2019-01-03 11:02:19 +0200

    Changes base on https://github.com/DbUp/DbUp/pull/335 https://github.com/treesrc/DbUp/commit/6dda3900ad9b5b1dd3ce78a0acd87740d9ba795a#diff-f19091f4624be7d8f6af18cd928f5cb2

--- src/dbup-tests/Support/Oracle/OracleConnectionManagerTests.cs
+++ src/dbup-tests/Support/Oracle/OracleConnectionManagerTests.cs
@@ -25,7 +25,7 @@ namespace DbUp.Tests.Support.Oracle
         [Fact]
         public void CanParseMultilineScript()
         {
-            var multiCommand = "create table FOO (myid INT NOT NULL);";
+            var multiCommand = "create table FOO (myid INT NOT NULL);/";
             multiCommand += Environment.NewLine;
             multiCommand += "create table BAR (myid INT NOT NULL);";
 
